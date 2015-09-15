b32473f432fbfc2ae6dc3c51de0582aa0bd20d67
Kayne Khoury <kaynekhoury1@gmail.com>
2015-09-15 18:45:44 -0500

    subset, summary, and plots data are added.

---
+++ 02 Visualizations/plot5.R
@@ -0,0 +1,27 @@
+require("ggplot2")
+
+require("RCurl")
+
+require("jsonlite")
+
+require("extrafont")
+
+return
+
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null and age <=10"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+# Plot 5
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Titanic') +
+  labs(x="SURVIVED", y=paste("FARE")) +
+  layer(data=df, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_jitter(width=0.3, height=0)
+  )
--- 02 Visualizations/plots.R
+++ 02 Visualizations/plots.R
@@ -1,5 +1,16 @@
+require("ggplot2")
+
+require("RCurl")
+
+require("jsonlite")
+
+require("extrafont")
+
+return
+
 #select * from titanic
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+head(df)
 
 # Plot 1
 ggplot() + 
@@ -19,13 +30,12 @@ ggplot() +
 
 #Where sex is not null
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
+
 #Display summary
-summary(df)
+
 #Display subset
 tail(subset(df, select = c(FARE, SEX)))
 
-require(extrafont)
 
 # Plot 2
 ggplot() + 
